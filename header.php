5e35a8fb72c75605ca0775b082b137ecd77faa90
Jessica <jpg_jessica@hotmail.com>
2025-01-16 10:55:04 -0500

    styles de base

--- header.php
+++ header.php
@@ -7,8 +7,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet"
+    />
+
     <title>La Maison de la Culture du Pic Champlain</title>
     <link rel="stylesheet" href="<?php bloginfo('template_url')  ?>/style.css">
+    <link rel="stylesheet" href="./assets/styles/scss/main.scss" />
     <?php wp_head(); ?>
 </head>
 <body>
