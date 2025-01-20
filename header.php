eb9efca2cc2d6dfc0ace01ebef33430d71c45ccb
ClemeDigs <159574722+ClemeDigs@users.noreply.github.com>
2025-01-20 14:59:48 -0500

    header done :)

--- header.php
+++ header.php
@@ -7,25 +7,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet"
+    />
+    <script src="https://kit.fontawesome.com/4360743ebd.js" crossorigin="anonymous"></script>
     <title>La Maison de la Culture du Pic Champlain</title>
     <link rel="stylesheet" href="<?php bloginfo('template_url')  ?>/style.css">
+    <link rel="stylesheet" href="./assets/styles/scss/main.scss" />
     <?php wp_head(); ?>
 </head>
 <body>
 
-    <header>
+    <header class="header">
         <nav class="header-content">
             <div class="logo">
                 <a href="<?php echo site_url(); ?>">
-                    <img src="<?php bloginfo('template_url'); ?>/assets/img/logo.svg" alt="Logo">
+                    <img class="nav__logo" src="<?php bloginfo('template_url'); ?>/assets/img/logo_header.svg" alt="Logo">
                 </a>
             </div>
             <?php
-                /* wp_nav_menu( $arg = array (
+                wp_nav_menu( $arg = array (
                 'menu' => 'Header',
-                'menu_class' => 'main-navigation',
+                'menu_class' => 'header__nav',
                 'theme_location' => 'primary'
-                )); */
+                )); 
             ?>
         </nav>
     </header>
