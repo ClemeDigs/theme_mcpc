91c9d29f1d33c8ebf333ef0143267f661e822a7c
Jessica <jpg_jessica@hotmail.com>
2025-01-21 14:30:25 -0500

    mise a jour footer

--- footer.php
+++ footer.php
@@ -11,27 +11,27 @@
         </div>
         <div class="footer__div">
             <div>
-                <h2>CONTACT</h3>
-                    <p><i class="fa-solid fa-location-dot"></i>126, rue du Mont-Saint-Louis<br>
-                        Rimouski (Québec) G0L 1B0</p>
-                    <p><i class="fa-solid fa-phone"></i>418 736-5237</p>
-                    <p><i class="fa-regular fa-envelope"></i><a href="mailto:maisonculturebic@gmail.com">maisonculturebic@gmail.com</a></p>
+                <h2>CONTACT</h2>
+                <p><i class="fa-solid fa-location-dot"></i>126, rue du Mont-Saint-Louis<br>
+                    Rimouski (Québec) G0L 1B0</p>
+                <p><i class="fa-solid fa-phone"></i>418 736-5237</p>
+                <p><i class="fa-regular fa-envelope"></i><a href="mailto:maisonculturebic@gmail.com">maisonculturebic@gmail.com</a></p>
             </div>
             <div>
                 <div class="infolettre">
-                    <h2>INFOLETTRE</h3>
-                        <p><i class="icon-mail"></i> Inscris-toi à notre infolettre !</p>
-                        <form action="" method="post" class="newsletter-form">
-                            <input type="email" name="newsletter_email" placeholder="Adresse courriel">
-                            <button type="submit">Soumettre</button>
-                        </form>
+                    <h2>INFOLETTRE</h2>
+                    <p><i class="icon-mail"></i> Inscris-toi à notre infolettre !</p>
+                    <form action="" method="post" class="newsletter-form">
+                        <input type="email" name="newsletter_email" placeholder="Adresse courriel">
+                        <button type="submit">Soumettre</button>
+                    </form>
                 </div>
                 <div class="footer-social">
-                    <h2>NOUS SUIVRE</h3>
-                        <p>
-                            <a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
-                        </p>
+                    <h2>NOUS SUIVRE</h2>
+                    <p>
+                        <a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+                    </p>
                 </div>
             </div>
         </div>
--- functions.php
+++ functions.php
@@ -1,23 +1,48 @@
 <?php
 
-function create_posttype() {
+function create_posttype()
+{
 
-    register_post_type( 'hero',
+    register_post_type(
+        'hero',
         array(
             'labels' => array(
-                'name' => __( 'HERO' ),
-                'singular_name' => __( 'HERO' )
+                'name' => __('HERO'),
+                'singular_name' => __('HERO')
             ),
             'public' => true,
             'has_archive' => true,
             'menu_icon' => 'dashicons-info',
             'rewrite' => array('slug' => 'hero'),
             'show_in_rest' => true,
-            'supports' => array('title', 'image'),   
+            'supports' => array('title', 'image'),
         )
     );
 }
 
 add_action('init', 'create_posttype');
 
+?>
+
+<?php
+function create_posttype()
+{
+    register_post_type(
+        'contact',
+        array(
+            'labels' => array(
+                'name' => __('contact'),
+                'singular_name' => __('contact')
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'menu_icon' => 'dashicons-info',
+            'rewrite' => array('slug' => 'contact'),
+            'show_in_rest' => true,
+            'supports' => array('title', 'id'),
+        )
+    );
+}
+
+add_action('init', 'create_posttype');
 ?>
