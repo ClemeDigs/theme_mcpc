6a16b9bd567468bc9ac3efea353f42a5f7cedaad
ECloutClou <9662217@cegeplimoilou.ca>
2025-01-31 09:22:50 -0500

    mise à jour du block basic

--- templates/components/basic_block.php
+++ templates/components/basic_block.php
@@ -10,29 +10,29 @@ function bloc_section_image($image) {
     endif;
 }
 ?>
-
+ 
 <?php
 // Récupération des sections ACF
 $basic_block_sections = [
     get_field('basic-block_section_1'),
     get_field('basic-block_section_2')
 ];
-
+ 
 // Filtrer les sections vides
 $basic_block_sections = array_filter($basic_block_sections, function ($block) {
     return is_array($block) && (
-        !empty($block['block_image']) || 
-        !empty($block['image_position']) || 
-        !empty($block['block_arrow']) || 
-        !empty($block['block_title']) || 
-        !empty($block['block_text']) || 
+        !empty($block['block_image']) ||
+        !empty($block['image_position']) ||
+        !empty($block['block_arrow']) ||
+        !empty($block['block_title']) ||
+        !empty($block['block_text']) ||
         !empty($block['block_link'])
     );
 });
-
+ 
 if (!empty($basic_block_sections)) :
 ?>
-
+ 
 <section class="block__container">
     <div class="block__bonhomme">
         <?php afficher_bonhomme('block_choix_du_bonhomme'); ?>
@@ -40,7 +40,7 @@ if (!empty($basic_block_sections)) :
     <div class="block__sections">
         <?php foreach ($basic_block_sections as $block) : ?>
             <div class="block__section">
-                <?php if (isset($block['image_position']) && $block['image_position'] == 0) bloc_section_image($block['block_image']); ?>
+                    <?php if (isset($block['image_position']) && $block['image_position'] == 1) bloc_section_image($block['block_image']); ?>
                 <div class="block__content">
                     <div class="block__header">
                         <?php if (!empty($block['block_title'])) : ?>
@@ -54,10 +54,9 @@ if (!empty($basic_block_sections)) :
                         <p><?php echo esc_html($block['block_text']); ?></p>
                     <?php endif; ?>
                 </div>
-                <?php if (isset($block['image_position']) && $block['image_position'] == 1) bloc_section_image($block['block_image']); ?>
+                <?php if (isset($block['image_position']) && $block['image_position'] == 0) bloc_section_image($block['block_image']); ?>
             </div>
         <?php endforeach; ?>
     </div>
 </section>
-
 <?php endif; ?>
