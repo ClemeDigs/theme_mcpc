b19a3527969eb39c970d536c26843bd71b603db0
ECloutClou <9662217@cegeplimoilou.ca>
2025-01-31 16:21:11 -0500

    Mise à jour du block basic - prise 2

--- templates/components/basic_block.php
+++ templates/components/basic_block.php
@@ -10,14 +10,14 @@ function bloc_section_image($image) {
     endif;
 }
 ?>
- 
+
 <?php
 // Récupération des sections ACF
 $basic_block_sections = [
     get_field('basic-block_section_1'),
     get_field('basic-block_section_2')
 ];
- 
+
 // Filtrer les sections vides
 $basic_block_sections = array_filter($basic_block_sections, function ($block) {
     return is_array($block) && (
@@ -29,10 +29,10 @@ $basic_block_sections = array_filter($basic_block_sections, function ($block) {
         !empty($block['block_link'])
     );
 });
- 
+
 if (!empty($basic_block_sections)) :
 ?>
- 
+
 <section class="block__container">
     <div class="block__bonhomme">
         <?php afficher_bonhomme('block_choix_du_bonhomme'); ?>
@@ -40,15 +40,18 @@ if (!empty($basic_block_sections)) :
     <div class="block__sections">
         <?php foreach ($basic_block_sections as $block) : ?>
             <div class="block__section">
-                    <?php if (isset($block['image_position']) && $block['image_position'] == 1) bloc_section_image($block['block_image']); ?>
+                <?php if (isset($block['image_position']) && $block['image_position'] == 1) bloc_section_image($block['block_image']); ?>
                 <div class="block__content">
                     <div class="block__header">
                         <?php if (!empty($block['block_title'])) : ?>
                             <h2><?php echo esc_html($block['block_title']); ?></h2>
                         <?php endif; ?>
-                        <?php if (!empty($block['block_arrow']) && isset($block['block_arrow']['url'], $block['block_arrow']['alt'])) : ?>
-                            <img src="<?php echo esc_url($block['block_arrow']['url']); ?>" alt="<?php echo esc_attr($block['block_arrow']['alt']); ?>">
-                        <?php endif; ?>
+                        <?php 
+                        // Utilisation de la nouvelle fonction afficher_arrow
+                        if (!empty($block['block_arrow'])) :
+                            afficher_arrow($block['block_arrow']);
+                        endif;
+                        ?>
                     </div>
                     <?php if (!empty($block['block_text'])) : ?>
                         <p><?php echo esc_html($block['block_text']); ?></p>
@@ -59,4 +62,5 @@ if (!empty($basic_block_sections)) :
         <?php endforeach; ?>
     </div>
 </section>
+
 <?php endif; ?>
