d23ee82ae7a86163098a0beda569b74afedaf5d2
ClemeDigs <159574722+ClemeDigs@users.noreply.github.com>
2025-01-30 22:09:14 -0500

    Create new pages

---
+++ page-membership-login.php
@@ -0,0 +1,23 @@
+<?php
+/* 
+Template Name: Membership login
+*/
+?>
+
+<?php
+get_header();
+?>
+
+<main id="main" class="main">
+
+<?php
+echo do_shortcode('[swpm_login_form]');
+?>
+
+</main>
+
+
+<?php
+get_footer();
+?>
+
---
+++ page-registration.php
@@ -0,0 +1,22 @@
+<?php
+/* 
+Template Name: Registration
+*/
+?>
+
+<?php
+get_header();
+?>
+
+<main id="main" class="main">
+
+<?php
+echo do_shortcode('[swpm_registration_form]');
+?>
+
+</main>
+
+
+<?php
+get_footer();
+?>
