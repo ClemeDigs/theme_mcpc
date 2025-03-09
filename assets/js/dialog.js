5b539f22978ab344e28ceb3649f7c64d27bf06a3
ClemeDigs <159574722+ClemeDigs@users.noreply.github.com>
2025-03-08 19:10:29 -0500

    better slider

--- assets/js/dialog.js
+++ assets/js/dialog.js
@@ -7,10 +7,21 @@ window.addEventListener("load", function () {
 });
 
 // Sélection de toutes les modales et de tous les boutons close
+/**
+ * @type {HTMLElement[]}
+ */
 const dialogs = document.querySelectorAll(".dialog");
+
+/**
+ * @type {HTMLElement[]}
+ */
 const btnsClose = document.querySelectorAll(".btn-close");
 
-// Fonction qui ferme la modale en supprimant les attributs 'open' et 'closing'
+// Fonction qui ferme la modale en supprimant les attributs 'open' et 'closing' et en retirant l'écouteur d'événement 'animationend'
+/**
+ * @param {Event} event
+ * @returns {void}
+ */
 function closeDialog(event) {
   const dialog = event.target;
   dialog.removeAttribute("open");
@@ -18,13 +29,21 @@ function closeDialog(event) {
   dialog.removeEventListener("animationend", closeDialog);
 }
 
-// Fonction qui ajoute l'attribut 'closing' à la modale avant de la fermer
+// Fonction qui ajoute l'attribut 'closing' à la modale et appelle l'écouteur d'événement 'animationend' pour appeler la fonction closeDialog
+/**
+ * @param {HTMLElement} dialog
+ * @returns {void}
+ */
 function closingDialog(dialog) {
   dialog.setAttribute("closing", "");
   dialog.addEventListener("animationend", closeDialog);
 }
 
-// Gestion de l'ouverture et de la fermeture des modales au clic
+//Ajoute un écouteur d'événement sur la fenêtre pour gérer l'ouverture et la fermeture des modales au clic. Si l'élément cliqué a un attribut 'data-dialog', la fonction recherche la modale correspondante et l'ouvre.
+/**
+ * @param {MouseEvent} e - L'événement de clic déclenché par l'utilisateur.
+ * @returns {void}
+ */
 window.addEventListener("click", (e) => {
   const target = e.target;
   const dialogSelector = target.getAttribute("data-dialog");
@@ -37,34 +56,36 @@ window.addEventListener("click", (e) => {
         closingDialog(dialog);
       } else {
         dialog.setAttribute("open", "");
-
-        // Empêcher la fermeture immédiate en ajoutant un délai avant l'écouteur de fermeture
-        setTimeout(() => {
-          dialog.dataset.allowClose = "true";
-        }, 100);
       }
     }
   }
 });
 
 // Ajout des écouteurs d'événements sur chaque modale pour les fermer au clic à l'extérieur
+/**
+ * @returns {void}
+ */
 dialogs.forEach((dialog) => {
-  dialog.addEventListener("click", (e) => {
-    if (dialog.dataset.allowClose === "true") {
-      closingDialog(dialog);
-    }
+  dialog.addEventListener("click", () => {
+    closingDialog(dialog);
   });
 
-  // Empêcher la propagation du clic à l'intérieur de la modale
-  const children = dialog.querySelectorAll("*");
-  children.forEach((child) => {
-    child.addEventListener("click", (e) => {
+  // Empêcher la propagation de l'événement clic à l'intérieur de la modale
+  /**
+   * @type {HTMLElement[]}
+   */
+  const childrens = dialog.querySelectorAll("& > *");
+  childrens.forEach((children) => {
+    children.addEventListener("click", (e) => {
       e.stopImmediatePropagation();
     });
   });
 });
 
-// Ajout des écouteurs d'événements sur chaque bouton close
+// Ajout des écouteurs d'événements sur chaque bouton close pour fermer toutes les modales lorsqu'un bouton close est cliqué
+/**
+ * @returns {void}
+ */
 btnsClose.forEach((btnClose) => {
   btnClose.addEventListener("click", () => {
     dialogs.forEach((dialog) => {
